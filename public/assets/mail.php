c090d0a518d58a227c6c31ca649aa636e07fc537
Thatiana <cruz.thatiana@gmail.com>
2019-12-21 13:24:11 -0800

    laines site firt push

---
+++ public/assets/mail.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['textarea'];
+$formcontent=" From: $name \n  Message: $message";
+$recipient = "info.1wmedia@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
